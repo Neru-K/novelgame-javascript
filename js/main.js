98b47862771ea5f4e5832a9d094d0633b2c611d5
Neru-K <atokoam@gmail.com>
2022-10-15 22:43:39 +0900

    delete unnecessary functions

--- js/main.js
+++ js/main.js
@@ -1,36 +1,8 @@
 //読み込み時に実行される関数
 (function () {
-  //drawBGImage("/images/background/engawa_day.jpg");
-  //drawCharaImage("/images/character/patient/josei_11_a.webp");
   initRevealTextMessage("女の子「いい縁側ですね 気に入りました」");
   window.addEventListener("load", function () {
     const conversation = new StartConversation();
     conversation.init();
   });
 })();
-
-//背景の描画
-function drawBGImage(src) {
-  const canvas = document.querySelector("#background-layer");
-  const ctx = canvas.getContext("2d");
-  const img = new Image();
-  img.src = src;
-
-  img.addEventListener("load", (event) => {
-    ctx.drawImage(img, 0, 0);
-  });
-}
-
-//キャラクターの描画
-function drawCharaImage(src) {
-  const canvas = document.querySelector("#character-layer");
-  const bgcanvas = document.querySelector("#background-layer");
-  const ctx = canvas.getContext("2d");
-  const img = new Image();
-  img.src = src;
-
-  img.addEventListener("load", (event) => {
-    const posX = (bgcanvas.width - img.width) / 2;
-    ctx.drawImage(img, posX, 100);
-  });
-}
